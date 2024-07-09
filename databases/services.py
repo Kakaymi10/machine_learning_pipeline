d49ce12194a578c544d7bdf1d5fecf1fc745b873
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2024-07-10 00:04:20 +0200

    crud

--- databases/services.py
+++ databases/services.py
@@ -65,3 +65,82 @@ locations = [
 
 for location in locations:
     add_location(*location)
+    
+def get_user(user_id):
+    db = get_database_connection()
+    if db:
+        cursor = db.cursor()
+        try:
+            cursor.execute('USE water_quality')
+            cursor.execute("""
+                SELECT * FROM user WHERE user_id = %s
+            """, (user_id,))
+            result = cursor.fetchone()
+            if result:
+                return {
+                    'user_id': result[0],
+                    'name': result[1],
+                    'email': result[2],
+                    'profession': result[3]
+                }
+            else:
+                return None
+        except mysql.connector.Error as err:
+            print(f"Failed to get user: {err}")
+        finally:
+            cursor.close()
+            db.close()
+    else:
+        print("Failed to connect to the database.")
+
+def update_user(user_id, name=None, email=None, profession=None):
+    db = get_database_connection()
+    if db:
+        cursor = db.cursor()
+        try:
+            cursor.execute('USE water_quality')
+            query = "UPDATE user SET "
+            params = []
+            if name:
+                query += "name = %s, "
+                params.append(name)
+            if email:
+                query += "email = %s, "
+                params.append(email)
+            if profession:
+                query += "profession = %s, "
+                params.append(profession)
+            query = query.rstrip(", ")
+            query += " WHERE user_id = %s"
+            params.append(user_id)
+            cursor.execute(query, params)
+            db.commit()
+            print(f"User '{name}' updated successfully.")
+        except mysql.connector.Error as err:
+            print(f"Failed to update user: {err}")
+        finally:
+            db.commit()
+            cursor.close()
+            db.close()
+    else:
+        print("Failed to connect to the database.")
+
+def delete_user(user_id):
+    db = get_database_connection()
+    if db:
+        cursor = db.cursor()
+        try:
+            cursor.execute('USE water_quality')
+            cursor.execute("""
+                DELETE FROM user WHERE user_id = %s
+            """, (user_id,))
+            db.commit()
+            print(f"User '{user_id}' deleted successfully.")
+        except mysql.connector.Error as err:
+            print(f"Failed to delete user: {err}")
+        finally:
+            db.commit()
+            cursor.close()
+            db.close()
+    else:
+        print("Failed to connect to the database.")
