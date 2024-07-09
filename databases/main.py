5e363ecf5a2708875b9fcd98fead2c755324f63d
kayc0des <k.boafo@alustudent.com>
2024-07-09 15:40:34 +0200

    succesfully connect to db

--- databases/main.py
+++ databases/main.py
@@ -1,5 +1,6 @@
-# import modules
-
+'''
+This Script connects to a remote mysql server and 
+'''
 import mysql.connector
 import os
 
@@ -8,12 +9,13 @@ user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
 database = os.getenv('DB_NAME')
 
-
-database = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
-
-print(database)
+try:
+    database = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    print(f'Succesfully connected to Database: {database}')
+except Exception as err:
+    print(f'An error occured: {err}')
