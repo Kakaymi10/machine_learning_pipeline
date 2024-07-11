3c737f6037fbede1555cdd4cc701e18bb9da3ec1
ell-sie <e.ndiramiye@alustudent.com>
2024-07-11 22:08:43 +0200

    creating endpoints

--- apis/database.py
+++ apis/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = ""
+# Corrected driver name: "mysql+pymysql"
+DATABASE_URL = "mysql+pymysql://root:Waterquality01!@kayc0des.com/u58951209_waterquality"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
--- apis/main.py
+++ apis/main.py
@@ -30,4 +30,39 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/locations/", status_code = status.HTTP_201_CREATED)
+async def create_locations(location: LocationBase, db: db_dependency):
+    db_location = models.User(**location.dict())
+    db.add(db_location)
+    db.comit()
+
+@app.get("/locations/{location_id}", status_code = status.HTTP_200_OK)
+async def read_location(location_id: int, db: db_dependency):
+    location= db.query(models.Location).filter(models.Location.id == location_id).first()
+    if location is None:
+        raise HTTPException(status_code=404, detail= 'Location not found')
+    return location
+
+@app.delte("/locations/{location_id}", status_code = status.HTTP_200_OK)
+async def delete_location(location_id: int, db: db_dependency):
+    db_location= db.query(models.Location).filter(models.Location.id == location_id).first()
+    if db_location is None:
+        raise HTTPException(status_code=404, detail= 'Location not found')
+    db_location(db_location)
+    db.commit()
+
+@app.post("/users/", status_code = status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.comit()
+
+@app.get("/users/{user_id}", status_code = status.HTTP_200_OK)
+async def read_user(user_id: int, db: db_dependency):
+    user= db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail= 'User not found')
+    return user
+
  
