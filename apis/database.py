db640a27b9f0147841527d713ddf10ae762d1fe8
Elsie Agasaro Ndiramiye <e.ndiramiye@alustudent.com>
2024-07-12 09:59:51 +0200

    refactoring

--- apis/database.py
+++ apis/database.py
@@ -2,13 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Replace the missing parts with the actual values
 username = "u58951209_group1"
 host = "kayc0des.com"
 database_name = "u58951209_waterquality"
 password = "Waterquality01!"
 
-# Construct the database URL
 DATABASE_URL = f"mysql+pymysql://{username}:{password}@{host}/{database_name}"
 
 engine = create_engine(DATABASE_URL)
